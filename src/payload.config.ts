9ff7b3ac88a5965f17d0519af3c2ebe5af2ce9b9
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2023-04-02 10:15:11 +0200

    fix server side

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -4,7 +4,7 @@ import Users from './collections/Users';
 import Studios from "./collections/Studios";
 
 export default buildConfig({
-  serverURL: 'http://localhost:3000',
+  serverURL: process.env.PAYLOAD_URL,
   admin: {
     user: Users.slug,
   },
