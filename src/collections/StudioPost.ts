06ccba2a6c2f17440a5c6701c09de1ebaefcbbd8
oliviervd <olliviervandhuynslager@gmail.com>
2023-04-08 10:13:11 +0200

    update studioPosts (add status), restructure layout

--- src/collections/StudioPost.ts
+++ src/collections/StudioPost.ts
@@ -2,13 +2,20 @@ import {CollectionConfig} from "payload/types";
 import titles from "../fields/titles";
 import textBlock from "../fields/textBlock";
 import postCategory from "../fields/postCategory";
+import contentStatus from "../fields/contentStatus";
 
 const StudioPosts: CollectionConfig = {
     slug: 'studioPost',
     fields:[
         titles,
         textBlock,
-        postCategory
+        {
+            type: "row",
+            fields: [
+                postCategory,
+                contentStatus
+            ]
+        }
     ]
 }
 
