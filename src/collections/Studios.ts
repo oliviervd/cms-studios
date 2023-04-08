6d03b3fc874737906273352913a19fe523fc995e
oliviervd <olliviervandhuynslager@gmail.com>
2023-04-08 10:40:15 +0200

    update access for published posts

--- src/collections/Studios.ts
+++ src/collections/Studios.ts
@@ -3,12 +3,17 @@ import titles from "../fields/titles";
 import textBlock from "../fields/textBlock"
 import postCategory from "../fields/postCategory";
 
+//todo: make studio labels global?
 const Studios: CollectionConfig = {
     slug: 'studios',
+    access: {
+        read: () => true
+    },
     fields: [
         //titles
         titles,
         // introduction text (short)
+        //todo: add media
         textBlock,
         // category
         postCategory
