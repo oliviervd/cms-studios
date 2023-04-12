3a12d02058b8b436575e94b64a3e2a5791b78037
oliviervd <olliviervandhuynslager@gmail.com>
2023-04-12 09:34:54 +0200

    setup env

--- src/collections/Admins.ts
+++ src/collections/Admins.ts
@@ -1,23 +1,15 @@
 import {CollectionConfig} from "payload/types";
+import {isAdmin} from "../access/isAdmin";
 
 const Admins: CollectionConfig = {
     slug: 'admins',
+    access: {
+        read: isAdmin,
+        update: isAdmin,
+
+    },
     fields: [
-        {
-            name: 'email',
-            type: 'email',
-            required:true
-        },
-        {
-            name: 'role',
-            type: 'select',
-            required: true,
-            options: [
-                'user',
-                'admin',
-                'editor'
-            ]
-        }
+
     ]
 }
 
