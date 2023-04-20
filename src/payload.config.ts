c293b450066fe63350e6452fa79cf19d29f80fb0
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2023-04-20 14:02:42 +0200

    add CSRF

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -9,28 +9,33 @@ import Categories from "./collections/Categories";
 import Media from "./collections/Media";
 
 export default buildConfig({
-  serverURL: process.env.PAYLOAD_URL,
-  cors: [
-      'https://dmg-programstudios.herokuapp.com',
-      'http://localhost:3000',
-      'https://p01--admin-cms--qbt6mytl828m.code.run/'
-  ], // String[] !
-  admin: {
-    user: Users.slug,
-  },
-  collections: [
-      Users,
-      Studios,
-      StudioPost,
-      studioEvent,
-      Categories,
-      Agents,
-      Media
-  ],
-  typescript: {
-    outputFile: path.resolve(__dirname, 'payload-types.ts'),
-  },
-  graphQL: {
-    schemaOutputFile: path.resolve(__dirname, 'generated-schema.graphql'),
-  },
+    serverURL: process.env.PAYLOAD_URL,
+    cors: [
+        'https://dmg-programstudios.herokuapp.com',
+        'http://localhost:3000',
+        'https://p01--admin-cms--qbt6mytl828m.code.run/'
+    ],
+    csrf: [
+        'https://dmg-programstudios.herokuapp.com',
+        'http://localhost:3000',
+        'https://p01--admin-cms--qbt6mytl828m.code.run/'
+    ],
+    admin: {
+        user: Users.slug,
+    },
+    collections: [
+        Users,
+        Studios,
+        StudioPost,
+        studioEvent,
+        Categories,
+        Agents,
+        Media
+    ],
+    typescript: {
+        outputFile: path.resolve(__dirname, 'payload-types.ts'),
+    },
+    graphQL: {
+        schemaOutputFile: path.resolve(__dirname, 'generated-schema.graphql'),
+    },
 });
