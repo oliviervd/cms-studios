19e37df383bf1c40adcf795ab03734b0c8fbdbea
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2023-05-25 12:34:57 +0200

    #1 add adapter for external S3 cloud storage.

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,5 +1,6 @@
 import { buildConfig } from 'payload/config';
 import { cloudStorage } from '@payloadcms/plugin-cloud-storage';
+import { s3Adapter } from '@payloadcms/plugin-cloud-storage/s3';
 
 import path from 'path';
 import Users from './collections/Users';
