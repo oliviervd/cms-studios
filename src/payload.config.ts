57d661a4055c41f786fd68bc004f915aa57d7077
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2023-05-26 17:35:54 +0200

    #1 add adapter for external S3 cloud storage.

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -29,8 +29,17 @@ export default buildConfig({
         cloudStorage({
             collections: {
                 'media': {
-                    adapter: adapter,
-                },
+                    adapter: s3Adapter({
+                        config: {
+                            endpoint: process.env.S3_ENDPOINT,
+                            credentials: {
+                                accessKeyId: process.env.S3_ACCESS_KEY_ID,
+                                secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+                            },
+                        },
+                        bucket: process.env.S3_BUCKET,
+                    }),
+                }
             },
         }),
     ],
