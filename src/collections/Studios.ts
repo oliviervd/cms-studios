c9ce0d641e4b1c8395f8956c17f6dc90935be58a
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2023-08-30 13:38:24 +0200

    add id.

--- src/collections/Studios.ts
+++ src/collections/Studios.ts
@@ -1,5 +1,4 @@
 import { CollectionConfig } from "payload/types";
-import titles from "../fields/titles";
 import textBlock from "../fields/textBlock"
 import postCategory from "../fields/postCategory";
 import {isAdmin} from "../access/isAdmin";
