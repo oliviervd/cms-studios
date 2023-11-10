17317b743d2f874323b1c1f710828346d0c5ef91
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2023-11-10 15:21:14 +0100

    add roles for studio grafisch ontwerp

--- src/collections/studio-graphic/StudioGraphicOutputTypes.ts
+++ src/collections/studio-graphic/StudioGraphicOutputTypes.ts
@@ -3,28 +3,19 @@ import { CollectionConfig } from "payload/types";
 const StudioGraphicOutputType: CollectionConfig = {
   slug: "output type",
   admin: {
-    group: "Studio Grafisch Ontwerp",
+    group: "Studio Graphisch Ontwerp",
+    useAsTitle: "type",
     description:
       "collection of types of output produced by the Studio Grafisch Ontwerp. Used to classify and filter.",
+  },
+  access: {
+    create: () => true,
+    read: () => true,
   },
   fields: [
     {
       name: "type",
       type: "text",
-    },
-    {
-      name: "dimensions",
-      type: "group",
-      fields: [
-        {
-          name: "height",
-          type: "text",
-        },
-        {
-          name: "width",
-          type: "text",
-        },
-      ],
     },
   ],
 };
