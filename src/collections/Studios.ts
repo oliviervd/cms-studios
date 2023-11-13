045b43e9ea0068aa09cf0b40c919b0c1b48e435d
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2023-11-13 18:56:30 +0100

    group globals

--- src/collections/Studios.ts
+++ src/collections/Studios.ts
@@ -1,61 +1,64 @@
 import { CollectionConfig } from "payload/types";
-import textBlock from "../fields/textBlock"
+import textBlock from "../fields/textBlock";
 import postCategory from "../fields/postCategory";
-import {isAdmin} from "../access/isAdmin";
-import {isEditor} from "../access/isEditor";
-import {hasAccessOrPublished} from "../access/hasAccessOrPublished";
+import { isAdmin } from "../access/isAdmin";
+import { isEditor } from "../access/isEditor";
+import { hasAccessOrPublished } from "../access/hasAccessOrPublished";
 import publishedOn from "../fields/publishedOn";
 import contentStatus from "../fields/contentStatus";
 
 //todo: make studio labels global?
 const Studios: CollectionConfig = {
-    slug: 'studios',
-    admin: {
-        useAsTitle: "EN",
-        description: "collection containing basic on the four active studios including (title, short description, thumbnail (cover image))"
+  slug: "studios",
+  admin: {
+    useAsTitle: "EN",
+    group: "globals",
+    description:
+      "collection containing basic on the four active studios including (title, short description, thumbnail (cover image))",
+  },
+  access: {
+    read: hasAccessOrPublished,
+    create: isAdmin,
+    delete: isAdmin,
+    update: isEditor,
+  },
+  fields: [
+    {
+      name: "URI",
+      label: "URI",
+      type: "text",
+      required: true,
     },
-    access: {
-        read: hasAccessOrPublished,
-        create: isAdmin,
-        delete: isAdmin,
-        update: isEditor
+    //todo: make group.
+    {
+      name: "EN",
+      label: "Title (ENGLISH)",
+      type: "text",
+      required: true,
     },
-    fields: [
-        {
-            name: "URI",
-            label: "URI",
-            type: "text",
-            required: true
-        },
-        //todo: make group.
-        {
-            name: 'EN',
-            label: "Title (ENGLISH)",
-            type: "text",
-            required: true
-        },
-        {
-            name: 'NL',
-            label: "Title (DUTCH)",
-            type: "text",
-            required: true
-        },
-        {
-            name: 'FR',
-            label: 'Title (FRENCH)',
-            type: 'text',
-            required: true
-        },
-        {
-            name:'studioImage',
-            type: "relationship",
-            relationTo: 'media'
-        },
-        textBlock,
-        postCategory,
-        publishedOn,
-        contentStatus
-    ]
-}
+    {
+      name: "NL",
+      label: "Title (DUTCH)",
+      type: "text",
+      required: true,
+    },
+    {
+      name: "FR",
+      label: "Title (FRENCH)",
+      type: "text",
+      required: true,
+    },
+    {
+      name: "studioImage",
+      type: "relationship",
+      relationTo: "media",
+    },
+    textBlock,
+    postCategory,
+    publishedOn,
+    contentStatus,
+  ],
+};
 
-export default Studios
+export default Studios;
+
