643219018d62bc1d214cf0f4a1b755c062b3b975
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2023-11-22 10:58:43 +0100

    hide collection

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -74,7 +74,7 @@ export default buildConfig({
     StudioGraphicOutput,
     StudioGraphicOutputType,
     StudioGraphicMembers,
-    StudioGraphicAbout,
+    //StudioGraphicAbout,
     //Categories,
     //Agents,
     Media,
