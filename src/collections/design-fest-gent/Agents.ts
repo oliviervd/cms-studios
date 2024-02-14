d18c029919f46795217abf255e7e90b848e471b8
oliviervd <oliviervandhuynslager@gmail.com>
2024-02-14 20:16:51 +0100

    add new collection for Design Fest Gent: Agents.ts

---
+++ src/collections/design-fest-gent/Agents.ts
@@ -0,0 +1,59 @@
+import {CollectionConfig} from "payload/types";
+
+const Agents:CollectionConfig = {
+    slug: "agents",
+    admin: {
+        group: "Design Fest Gent",
+    },
+    access : {
+        read: () => true,
+        update: () => true,
+        delete: () => true,
+        create: () => true,
+    },
+    fields: [
+        {
+            type:"row",
+            fields: [
+                {
+                    name: "firstName",
+                    label: {
+                        en: "first name",
+                        nl: "voornaam",
+                        fr: "prénom"
+                    },
+                    type: "text"
+                },
+                {
+                    name: "familyName",
+                    label: {
+                        en: "surname",
+                        nl: "familienaam",
+                        fr: "nom de famille"
+                    },
+                    type: "text"
+                }
+            ]
+        },
+        {
+            name:"biography",
+            label: {
+                en: "biography",
+                nl: "biografie",
+                fr: "biographie"
+            },
+            type: "richText",
+            localized: true
+        },
+        {
+            name: "website",
+            type: "text",
+            label: {
+                en: "website",
+                fr: "website",
+                nl: "webpagina"
+            }
+        }
+    ]
+}
+export default Agents;
