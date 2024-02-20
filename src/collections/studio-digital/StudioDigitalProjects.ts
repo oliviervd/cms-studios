c7e95d59197116039d12d73cd55f08d4eec0003b
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2024-02-20 12:03:28 +0100

    extend collection StudioDigitalProjects.ts

--- src/collections/studio-digital/StudioDigitalProjects.ts
+++ src/collections/studio-digital/StudioDigitalProjects.ts
@@ -17,12 +17,31 @@ const StudioDigitalProjects:CollectionConfig = {
             name: "projectTitle",
             label: {
                 "en": "title",
-                "nl": "titel"
+                "nl": "titel",
+                "fr": "titre"
             },
             type: "text"
+        },
+        {
+            name: "projectDescription",
+            label: {
+                "en": "description",
+                "nl": "beschrijving",
+                "fr": "description"
+            },
+            localized: true,
+            type: "richText"
+        },
+        {
+            name: "active",
+            label: {
+                "en": "active",
+                "nl": "actief",
+                "fr": "en cours"
+            },
+            type: "checkbox"
         }
     ]
-
 }
 
 export default StudioDigitalProjects
