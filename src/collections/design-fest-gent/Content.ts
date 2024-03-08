626a75c1a4dc64ec9d78e7f1499f718fc5fb6285
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2024-03-08 09:30:23 +0100

    add content collection (for generating content using Blocks)

--- src/collections/design-fest-gent/Content.ts
+++ src/collections/design-fest-gent/Content.ts
@@ -1,73 +1,24 @@
 import {CollectionConfig} from "payload/types";
+import MainTitleBlock from "../../blocks/Title";
 
 const Content:CollectionConfig = {
-    slug:"content",
-    admin: {group: "Design Fest Gent"},
-    access : {
-        read: () => true,
-        update: () => true,
-        delete: () => true,
-        create: () => true,
+    slug: "content",
+    admin: {
+        group: "design-fest-gent"
     },
     fields: [
         {
-            name: "id",
-            type:"text",
-            label: "id",
+            name: 'layout', // required
+            type: 'blocks', // required
+            minRows: 1,
+            maxRows: 20,
+            blocks: [
+                // required
+                MainTitleBlock,
+            ],
         },
-        {
-            name:"title",
-            label: {
-                en: "title",
-                fr: "titre",
-                nl: "titel"
-            },
-            localized: true,
-            type: "text"
-        },
-        {
-            name: "slug",
-            label: {
-                en: "summary/slug",
-                nl: "synopsis/slug",
-                fr: "slug"
-            },
-            type:"richText",
-            localized: true,
-        },
-        {
-            name: "text",
-            label: {
-                en: "text",
-                nl: "tekst",
-                fr: "text"
-            },
-            localized: true,
-            type: "richText"
-        },
-        {
-            name: "type",
-            label: "type",
-            type: "select",
-            options: [
-                {
-                    label: {
-                        en: "about",
-                        nl: "over",
-                        fr: "a propos de"
-                    },
-                    value: "about"
-                },
-                {
-                    label: {
-                        en: "article",
-                        nl: "artikel",
-                        fr: "article"
-                    },
-                    value: "article"
-                }
-            ]
-        }
-    ]
+    ],
+
 }
-export default Content
+
+export default Content;
